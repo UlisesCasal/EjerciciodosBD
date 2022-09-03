fbb05804c5a8534dd566ce6b38e5d480a6f31574
Neielmonti <neielmonti@gmail.com>
2022-09-03 20:11:41 -0300

    Actualizacion 2
    Grabado de registros arreglado.

--- src/Main.java
+++ src/Main.java
@@ -5,32 +5,19 @@ public class Main {
     static RandomAccessFile archivo = null;
     public static void main(String[] args) throws IOException {
 
-
                 FileOutputStream fos = null;
-                DataOutputStream salida = null;
-
 
                 try {
                     //crea el archivo:
                     archivo = new RandomAccessFile("datos.bin", "rw");
 
-
-                    //crea puntero al registro.
-
                     //asigno registros:
-                    Registro registro1 = new Registro(0, "000000", "aaaaaaaaaa", "aaaaaaaaaa", 000000);
-                    Registro registro2 = new Registro(1, "000001", "bbbbbbbbbb", "bbbbbbbbbb", 000000);
-                    Registro registro3 = new Registro(2, "000002", "cccccccccc", "cccccccccc", 000000);
+                    Registro registro1 = new Registro(0,0, "aaaaaaaaaa", "bbbbbbbbbb", 0);
                     registro1.escribirRegistro(archivo);
-                    registro2.escribirRegistro(archivo);
-                    registro3.escribirRegistro(archivo);
-                    String linea = "";
-                    archivo.seek(0); //nos situamos al principio
-                    for (int i = 0; i < 36; i++) {
-                        linea += archivo.readChar();
-                    }
-                    System.out.println(linea);  //se muestra en pantalla
 
+                    //Recupero registro del archivo
+                    Registro leer = Registro.devolverRegistro(archivo, 0);
+                    System.out.println(leer.armarString());  //se muestra en pantalla
 
 
                 } catch (IOException e) {
@@ -39,9 +26,6 @@ public class Main {
                     try {
                         if (fos != null) {
                             fos.close();
-                        }
-                        if (salida != null) {
-                            salida.close();
                         }
                     } catch (IOException e) {
                         System.out.println(e.getMessage());
--- src/Registro.java
+++ src/Registro.java
@@ -1,14 +1,35 @@
 import java.io.IOException;
 import java.io.RandomAccessFile;
 
-public record Registro(Integer indice, String codigoCliente, String apellido, String nombre, Integer enlace) {
+public record Registro(Integer indice, Integer codigoCliente, String apellido, String nombre, Integer enlace) {
 
-    private String armarString() {
+    public String armarString() {
         return this.indice + ", " +  this.codigoCliente + ", " + this.apellido + ", " + this.nombre + ", " + this.enlace;
     }
 
+    // Escribir el contenido de un registro en un archivo pasado por parametro(temporalmente APPEND)
     public void escribirRegistro(RandomAccessFile archivo) throws IOException {
-        String linea = this.armarString();
-        archivo.writeChars(linea);
+        archivo.seek(archivo.length());
+        archivo.writeInt(this.indice);
+        archivo.writeInt(this.codigoCliente);
+        archivo.writeUTF(this.apellido);
+        archivo.writeUTF(this.nombre);
+        archivo.writeInt(this.enlace);
+        //System.out.println(archivo.getFilePointer());
+        //String linea = this.armarString();
+        //archivo.writeChars(linea);
     }
+
+    // Escribir en un registro el contenido de un archivo en una linea especificada por parametro
+    public static Registro devolverRegistro(RandomAccessFile archivo, Integer posicion) throws IOException {
+        archivo.seek(posicion*36);
+        Integer index = archivo.readInt();
+        Integer code = archivo.readInt();
+        String ape = archivo.readUTF();
+        String nom = archivo.readUTF();
+        Integer enl = archivo.readInt();
+        Registro rg = new Registro(index,code,ape,nom,enl);
+        return rg;
+    }
+
 }
