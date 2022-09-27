7838b38b7b55a59a86c68b71ad183d51ff4aa540
Nicolas Agustin Onofrio <83963460+Agustin-Onofrio@users.noreply.github.com>
2022-09-27 19:55:48 -0300

    Add files via upload

---
+++ EjerciciodosBD-master/EjerciciodosBD-master/src/Funciones.java
@@ -0,0 +1,257 @@
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.Scanner;
+
+import javax.swing.plaf.synth.SynthPasswordFieldUI;
+
+public class Funciones {
+
+    static final int inicioZO = 877;
+    static int elementosZO = 0;
+
+    String pausa;
+    Scanner sc =  new Scanner(System.in);
+
+    public static String hacerStringde10(String cadena) {
+        cadena = cadena.trim();
+        int agregar = 10 - cadena.length();
+        for (int i = 0; i < agregar; i++) {
+            cadena += " ";
+        }
+        return cadena;
+    }
+
+    public static void crearVacio(RandomAccessFile archivo) throws IOException {
+        //En caso que el archivo no exista, lo crea con todos en nulo
+        for (int i = 0; i < inicioZO; i++) {
+            Registro registroNulo = new Registro(i, 0, "..........", "..........", -1, false);
+            registroNulo.escribirRegistro(archivo);
+        }
+
+    }
+
+    public static void mostrarArchivo(RandomAccessFile archivo) throws IOException {
+        
+        archivo.seek(0);
+        Integer i=0;
+        Integer eof= (int) (archivo.length()/Registro.tamanioRegistro)+elementosZO;
+        Registro leer;
+        while(i< eof){
+            leer = Registro.devolverRegistro(archivo, i);
+            System.out.println(leer.armarString());   
+            i++;
+        }
+        
+    }
+
+    public static Integer buscarLibreZO(RandomAccessFile archivo) throws IOException {
+        int posicionLibre = -1;
+        int posicionActual = inicioZO;
+        boolean encontrado = false;
+        Registro aux;
+        while ((((long) posicionActual * Registro.tamanioRegistro) < archivo.length()) && (!encontrado)) {
+            aux = Registro.devolverRegistro(archivo, posicionActual);
+            if (aux.estado()) {
+                posicionActual ++;
+            }
+            else {
+                encontrado = true;
+                posicionLibre = posicionActual;
+            }
+        }
+        return posicionLibre;
+    }
+
+    public static Integer funcionHash(Integer numero) {
+        return numero % 877;
+    }
+
+
+
+    public static void alta(RandomAccessFile archivo, Registro registro) throws IOException {
+        int hashCode = funcionHash(registro.codigoCliente());
+        
+        Registro registroActual = Registro.devolverRegistro(archivo, hashCode);
+
+        if (!registroActual.estado()) {
+            registro.sobreescribirRegistro(archivo,hashCode);
+        } else {
+            int posicionRegistroActual = hashCode;
+            int posicionAModificar = posicionRegistroActual;
+
+            while ((registroActual.estado()) && (posicionRegistroActual != -1)) {
+                posicionRegistroActual = registroActual.enlace();
+                if (registroActual.enlace() != -1) {
+                    posicionAModificar = posicionRegistroActual;
+                    registroActual = Registro.devolverRegistro(archivo, registroActual.enlace());
+                }
+
+            }
+
+            int posicionEscribir = buscarLibreZO(archivo);
+
+            
+            
+
+            posicionEscribir = ((int) (archivo.length()) / Registro.tamanioRegistro)+elementosZO;
+            
+            if (posicionEscribir == -1) {
+                registro=Registro.modificarRegistro(posicionEscribir,registro.codigoCliente(),
+                                            registro.apellido(),registro.nombre(),-1,registro.estado());
+                registro.escribirRegistro(archivo);
+                elementosZO ++;
+            }
+            else {
+                registro=Registro.modificarRegistro(posicionEscribir,registro.codigoCliente(),
+                                            registro.apellido(),registro.nombre(),-1,registro.estado());
+                registro.sobreescribirRegistro(archivo,posicionEscribir);
+                
+            }
+
+            registroActual = Registro.modificarRegistro(registroActual.indice(),registroActual.codigoCliente(),
+                                            registroActual.apellido(),registroActual.nombre(),posicionEscribir,registroActual.estado());
+            registroActual.sobreescribirRegistro(archivo,posicionAModificar);
+
+        }
+
+    }
+
+//Busco registro y devuelvo posicion
+public static int buscarRegistro(RandomAccessFile archivo, int codigoCliente) throws IOException{
+    
+    int buscarRegistro=-1;
+    int indice = funcionHash(codigoCliente);
+
+    archivo.seek((long) indice * Registro.tamanioRegistro);
+    Integer index = archivo.readInt();
+    Integer code = archivo.readInt();
+    String ape = archivo.readUTF();
+    String nom = archivo.readUTF();
+    Integer enl = archivo.readInt();
+    boolean est = archivo.readBoolean();
+    if (codigoCliente==code && est){
+        buscarRegistro=index;
+    }else {
+        
+        while(est && enl!=-1){
+            archivo.seek((long) enl * Registro.tamanioRegistro);
+            index = archivo.readInt();
+            code = archivo.readInt();
+            ape = archivo.readUTF();
+            nom = archivo.readUTF();
+            enl = archivo.readInt();
+            est = archivo.readBoolean();
+
+            if (codigoCliente==code && est){
+                buscarRegistro=index;
+            }
+
+        }
+        
+    }
+
+
+    
+    return buscarRegistro;
+
+}
+
+
+
+
+public static boolean baja(RandomAccessFile archivo, int codigoCliente) throws IOException{
+    
+
+    int posicionRegistroAEliminar= funcionHash(codigoCliente);
+    
+
+    
+    Registro registroActual = Registro.devolverRegistro(archivo, posicionRegistroAEliminar);
+    
+    int posicionAnterior=posicionRegistroAEliminar;
+    int posicionActual= posicionRegistroAEliminar; 
+    int posicionSiguiente= registroActual.enlace();
+    
+    
+  
+        boolean encontro=false;
+        boolean zo=false;
+    do{
+        if(registroActual.codigoCliente()==codigoCliente){
+            encontro=true;
+        } else{
+            zo=true;
+            posicionAnterior=posicionActual;
+            posicionActual=posicionSiguiente;
+            if(posicionActual!=-1){
+                registroActual = Registro.devolverRegistro(archivo, posicionActual);
+            }
+            posicionSiguiente=registroActual.enlace();
+        } 
+        
+    }while(posicionActual!=-1 && !encontro);
+
+
+
+    
+   
+
+//si el elemento a eliminar es el primero de la lista enlazada
+
+if ( encontro && posicionSiguiente==-1){
+    Registro registroAnterior = Registro.devolverRegistro(archivo, posicionAnterior);
+    registroAnterior = new Registro(registroAnterior.indice(), registroAnterior.codigoCliente(), registroAnterior.apellido(), registroAnterior.nombre(), -1, registroAnterior.estado());
+    registroAnterior.sobreescribirRegistro(archivo, posicionAnterior);
+
+    registroActual = new Registro(posicionActual, 0, "..........", "..........", -1, false);
+    registroActual.sobreescribirRegistro(archivo, posicionActual);
+}else if(encontro && posicionSiguiente != -1){
+        do{
+
+            Registro registroSiguiente = Registro.devolverRegistro(archivo, posicionSiguiente);
+            posicionAnterior=posicionActual;
+            posicionActual=posicionSiguiente;
+            posicionSiguiente=registroSiguiente.enlace();
+            registroSiguiente = new Registro(registroActual.indice(), registroSiguiente.codigoCliente(), registroSiguiente.apellido(), registroSiguiente.nombre(), registroActual.enlace(), registroSiguiente.estado());
+            registroSiguiente.sobreescribirRegistro(archivo, posicionAnterior);
+            
+            
+           
+        }while(posicionSiguiente!=-1);
+
+        Registro registroAnterior = Registro.devolverRegistro(archivo, posicionAnterior);
+        registroAnterior = new Registro(registroAnterior.indice(), registroAnterior.codigoCliente(), registroAnterior.apellido(), registroAnterior.nombre(), -1, registroAnterior.estado());
+        registroAnterior.sobreescribirRegistro(archivo, posicionAnterior);
+
+        registroActual = new Registro(posicionActual, 0, "..........", "..........", -1, false);
+        registroActual.sobreescribirRegistro(archivo, posicionActual);
+
+
+    }
+
+
+
+    if(encontro && zo){
+        elementosZO--;
+    }
+
+    return  encontro;
+
+
+
+    
+}
+
+
+
+
+
+
+
+   
+}
+
+        
+
+
+//END.
---
+++ EjerciciodosBD-master/EjerciciodosBD-master/src/Main.java
@@ -0,0 +1,186 @@
+import java.io.*;
+import java.util.Scanner;
+
+import javax.swing.plaf.synth.SynthPasswordFieldUI;
+
+public class Main {
+    static RandomAccessFile archivo = null;
+    public static void main(String[] args) throws IOException {
+
+                FileOutputStream fos = null;
+
+                try {
+                    //crea el archivo:
+                    archivo = new RandomAccessFile("datos.bin", "rw");
+                    if (archivo.length() == 0) {
+                        Funciones.crearVacio(archivo);
+                        System.out.println("Archivo creado!!!");
+                    }
+                    
+                    //Funciones.mostrarArchivo(archivo);
+                    archivo.close();
+
+
+
+                    Scanner sc = new Scanner(System.in);
+                    String opcion;
+                    int codigoCliente;
+                    String apellido,nombre,pausa;
+                    do {
+                        //System.out.println("Presione enter para continuar...");
+                        //String pausa = sc.nextLine();
+                        System.out.println("-------------------Elegi opcion-----------------------");
+                        System.out.println("1.Alta de registro");
+                        System.out.println("2.Baja de registro");
+                        System.out.println("3.Actualizacion de registro");
+                        System.out.println("4.Buscar registro");
+                        System.out.println("5.Mostrar archivo");
+                        System.out.println("0. Salir");
+                        opcion = sc.nextLine();
+                        switch (opcion) {
+                        case "1":
+                            System.out.println("Ingresa el codigo de cliente: ");
+                            codigoCliente = sc.nextInt();
+                            
+
+                            archivo = new RandomAccessFile("datos.bin", "rw");
+
+                            if (Funciones.buscarRegistro(archivo, codigoCliente)<0){
+                                pausa = sc.nextLine();
+                                System.out.println("Ingresa el apellido del cliente");
+                                apellido = sc.nextLine();
+                                pausa = sc.nextLine();
+                                System.out.println("Ingresa el nombre del cliente");
+                                nombre = sc.nextLine();
+                                Registro reg = new Registro(Funciones.funcionHash(codigoCliente),codigoCliente,
+                                Funciones.hacerStringde10(apellido),Funciones.hacerStringde10(nombre), -1,true);
+                                Funciones.alta(archivo,reg);
+                            }else {
+                                
+                                System.out.println("El codigo de cliente ya existe...");
+                                pausa = sc.nextLine();
+                            }
+                            archivo.close();
+                            
+                            
+                            pausa = sc.nextLine();
+                            break;
+                        case "2":
+                            System.out.println("Ingresa el codigo de cliente: ");
+                            codigoCliente = sc.nextInt();
+                            
+                            archivo = new RandomAccessFile("datos.bin", "rw");
+                            if(Funciones.baja(archivo,codigoCliente)){
+                                System.out.println("Se elimino el registro correctamente");
+                            }else{
+                                System.out.println("No ingresaste un codigo existente...");
+                            };
+                            archivo.close();
+                            pausa = sc.nextLine();
+                            break;
+                        case "3":
+                            boolean opcionValida=false;
+                            System.out.println("Ingresa el codigo de cliente: ");
+                            codigoCliente = sc.nextInt();
+
+                            archivo = new RandomAccessFile("datos.bin", "rw");
+                            int posicionAModificar=Funciones.buscarRegistro(archivo, codigoCliente);
+                            if (posicionAModificar>=0){
+
+                                while(!opcionValida){
+                                    System.out.println("-------------------Elegi opcion-----------------------");
+                                    System.out.println("1.Cambiar nombre");
+                                    System.out.println("2.Cambiar apellido");
+                                    System.out.println("0.Cancelar");
+                                    Registro registroAModificar;
+                                    switch (opcion) {
+                                        case "1":
+                                        //cambiarNombre
+                                        System.out.println("Ingresa el nombre del cliente");
+                                        nombre = sc.nextLine();
+                                        registroAModificar = Registro.devolverRegistro(archivo, posicionAModificar);
+                                        registroAModificar = new Registro(registroAModificar.indice(),registroAModificar.codigoCliente(),
+                                            Funciones.hacerStringde10(registroAModificar.apellido()),Funciones.hacerStringde10(nombre), registroAModificar.enlace(),true);
+
+                                        registroAModificar.sobreescribirRegistro(archivo, posicionAModificar);
+                                        opcionValida=true;
+                                        break;
+                                        case "2":
+                                        //cambiar apellido
+                                        System.out.println("Ingresa el apellido del cliente");
+                                        apellido = sc.nextLine();
+                                        registroAModificar = Registro.devolverRegistro(archivo, posicionAModificar);
+                                        registroAModificar = new Registro(registroAModificar.indice(),registroAModificar.codigoCliente(),
+                                            Funciones.hacerStringde10(apellido),Funciones.hacerStringde10(registroAModificar.nombre()), registroAModificar.enlace(),true);
+
+                                        registroAModificar.sobreescribirRegistro(archivo, posicionAModificar);
+                                        opcionValida=true;
+                                        break;
+                                        case "0":
+                                        opcionValida=true;
+                                        //salir
+                                    }
+                                }                   
+                                pausa = sc.nextLine();
+                            }else {
+                                
+                                System.out.println("El codigo de cliente no existe...");
+                                pausa = sc.nextLine();
+                            }
+                            
+                            archivo.close();
+                            pausa = sc.nextLine();
+                            break;
+                        case "4":
+
+                            System.out.println("Ingresa el codigo de cliente: ");
+                            codigoCliente = sc.nextInt();
+                            archivo = new RandomAccessFile("datos.bin", "rw");
+                            int indice=Funciones.buscarRegistro(archivo, codigoCliente);
+                            if(indice>=0) {
+                                Registro registroBuscado = Registro.devolverRegistro(archivo,indice);
+                                System.out.printf("Registro : ");
+                                System.out.println(registroBuscado.armarString());
+                                pausa= sc.nextLine();
+                            }else{
+                                System.out.println("El codigo de cliente buscado no existe");
+                            }
+
+                            archivo.close();
+
+                            pausa= sc.nextLine();
+                            pausa= sc.nextLine();
+                            break;
+                        case "5":
+                            archivo = new RandomAccessFile("datos.bin", "rw");
+                            Funciones.mostrarArchivo(archivo);
+                            archivo.close();
+                            pausa = sc.nextLine();
+                            break;
+                        case "0":
+                            System.out.println("Excelente servicio, nos vemos");
+                            //pausa = sc.nextLine();
+                            break;
+                        default:
+                            System.out.println("Opcion incorrecta");
+
+                            break;
+                        }
+                    }while(!(opcion.equals("0")));
+    
+
+
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                } finally {
+                    try {
+                        if (fos != null) {
+                            fos.close();
+                        }
+                    } catch (IOException e) {
+                        System.out.println(e.getMessage());
+                    }
+                }
+
+    }
+}
